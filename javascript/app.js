949afcc3c85b7e1cef1bf1dedcb3a97de380b4b5
Firas Hasan <x.firashasan@gmail.com>
2021-04-06 02:58:13 +0300

    lab07

--- javascript/app.js
+++ javascript/app.js
@@ -4,8 +4,86 @@ function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+let total=0;
 
+function MakingTables(name, minCustomer, maxCustomer, avgCookieSale)
+{
+    this.name = name;
+    this.minCustomer = minCustomer;
+    this.maxCustomer = maxCustomer;
+    this.avgCookieSale = avgCookieSale;
+    // this.randomCustomer=randomCustomer;
+    // this.randomAverage=randomAverage;
+    // this.total=total;
+}
+
+MakingTables.prototype.randomCustomer= function ()
+{
+    for (let i = 0; i < hours.length; i++)
+    {
+        this.randomCustomer.push(randomNumber(this.minCustomer, this.maxCustomer));
+    }
+    
+};
+MakingTables.prototype.avgCookies= function ()
+{
+    for (let i = 0; i < this.randomCustomer.length; i++)
+    {   let multiply= this.randomCustomer[i]*this.avgCookieSale;
+        multiply=Math.floor(multiply);
+        this.randomAverage.push(multiply);
+        this.total=this.total+this.randomAverage[i];
+    } 
+    
+};
+MakingTables.prototype.render= function()
+{
+    let coolTables=document.getElementById('cool-tables');
+    console.log(coolTables);
+    let theTable = document.createElement('table');
+    parent.appendChild(theTable);
+    let headRow=document.createElement('tableRow');
+    theTable.appendChild(headRow);
+    for(let i=0; i<hours.length;i++)
+    {
+        let tableHead=document.createElement("tableHeader");
+        headRow.appendChild(theTable);
+        tableHead.textContent=hours[i];
+    }
+}
+
+// function Table()
+// {   
+//     let theTable = document.createElement('table');
+//     parent.appendChild(theTable);
+//     let headRow=document.createElement('tableRow');
+//     theTable.appendChild(headRow);
+//     for(let i=0; i<hours.length;i++)
+//     {
+//         let tableHead=document.createElement("tableHeader");
+//         headRow.appendChild(theTable);
+//         tableHead.textContent=hours[i];
+//     }
+// }
+
+
+let seatle= new MakingTables('seatle',23,65,6.3);
+let tokyo= new MakingTables('tokyo',3,24,1.2);
+let dubai= new MakingTables('dubai',11,38,3.7);
+let paris= new MakingTables('paris',20,38,2.3);
+let lima= new MakingTables('lima',2,16,4.6);
+console.log(seatle);
+console.log(tokyo);
+console.log(dubai);
+console.log(paris);
+console.log(lima);
+
+
+
+
+
+
+/*
 let seatle = {
     name:'seatle',
     minCustomer: 23, 
